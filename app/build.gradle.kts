b522f2a4945b5c2f4e3618e28e282a4f7474f74e
jamielcs <108922366+jamielcs@users.noreply.github.com>
2026-09-15 02:36:21 +0700

    Vir Vid 7: vendor camera API scanner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "id.armagic.virvid5"
+        applicationId = "id.armagic.virvid7"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 7
+        versionName = "1.7.0"
     }
 
     buildFeatures {
@@ -33,6 +33,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.media3:media3-exoplayer:1.5.1")
-    implementation("androidx.media3:media3-ui:1.5.1")
+
+    implementation("dev.rikka.shizuku:api:13.1.5")
+    implementation("dev.rikka.shizuku:provider:13.1.5")
 }
