cb29998aaad4eb6e2888ed5992192fc701807bb7
jamielcs <108922366+jamielcs@users.noreply.github.com>
2026-09-15 01:03:12 +0700

    V3: prepare Vir Vid 3 standalone install

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "id.armagic.virtualvideo"
+        applicationId = "id.armagic.virvid3"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     buildFeatures {
